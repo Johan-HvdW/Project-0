f97b96f1870a971f39d6c25130561b1c9202fc57
Xavi Vega <xabi1309@gmail.com>
2022-11-27 09:52:06 +0100

    Add project structure

---
+++ tests/test_simple.py
@@ -0,0 +1,10 @@
+import unittest
+from simple import add_one 
+
+class TestSimple(unittest.TestCase):
+    def test_add_one(self):
+        self.assertEqual(add_one(1), 2)
+
+
+if __name__ == '__main__':
+    unittest.main()
